3fb5d6adf725afcccff2e2e130ae44c5cec82192
Makos <makos4608@gmail.com>
2020-10-12 18:33:35 +0200

    Create component to show the temperature of the next hours. Create component to show single hour.

---
+++ src/components/BoxForHourTemperature.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+
+const TempBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 15px;
+`;
+
+const Icon = styled.img`
+  margin: 5px 0;
+  width: 35px;
+`;
+
+const Sup = styled.sup`
+  margin-left: 2px;
+`;
+
+const BoxForHourTemperature = ({ dt, temp, weather, now }) => {
+  const hourValue = new window.Date(dt * 1000).toLocaleTimeString("pl-PL", {
+    hour: "numeric",
+    minute: "numeric",
+  });
+
+  const { icon } = weather[0];
+
+  const temperature = Math.ceil(temp);
+
+  return (
+    <TempBox>
+      <p>
+        {temperature}
+        <Sup>o</Sup>
+      </p>
+      <Icon src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="" />
+      <p>{now ? "NOW" : hourValue}</p>
+    </TempBox>
+  );
+};
+
+export default BoxForHourTemperature;
---
+++ src/components/FutureTemperatures.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import styled from "styled-components";
+
+import AppContext from "context/AppContext";
+
+import BoxForHourTemperature from "components/BoxForHourTemperature";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const FutureTemperatures = () => {
+  const { hourly } = useContext(AppContext);
+
+  const tempNow = hourly?.slice(0, 1);
+  const nextFiveHours = hourly?.slice(1, 6);
+
+  return (
+    <Wrapper>
+      {tempNow?.map((temp) => (
+        <BoxForHourTemperature key={temp.dt} {...temp} now />
+      ))}
+      {nextFiveHours?.map((hour) => (
+        <BoxForHourTemperature key={hour.dt} {...hour} />
+      ))}
+    </Wrapper>
+  );
+};
+
+export default FutureTemperatures;
