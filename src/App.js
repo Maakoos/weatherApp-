0c2352c0b3a7973537bdbd0171a986b871594253
Makos <makos4608@gmail.com>
2020-10-20 13:43:58 +0200

    Add error handling and logic to display error message.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AppContext from "context/AppContext";
 
 import LoadingBox from "components/LoadingBox";
 import BackgroundBox from "components/BackgroundBox";
+import ErrorBox from "components/ErrorBox";
 
 import MainView from "views/MainView";
 import DetailsView from "views/DetailsView";
@@ -19,18 +20,35 @@ function App() {
   const [futureWeather, setFutureWeather] = useState({});
 
   const [showLoader, setShowLoader] = useState(false);
+  const [showErrorBox, setShowErrorBox] = useState(null);
 
   const fetchData = async (city) => {
     setShowLoader(true);
-    const response = await axios(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}&lang=en`
-    );
 
-    setCurrentWeather(response.data);
+    let response = null;
+    try {
+      response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}&lang=en`
+      );
+      setCurrentWeather(response.data);
+    } catch (err) {
+      if (err.response.status === 404) {
+        setTimeout(() => {
+          setShowErrorBox(err.response.data);
+        }, 2000);
+        setFutureWeather({});
+        setCurrentWeather({});
+        return;
+      } else if (err.response.status === 500) {
+        setTimeout(() => {
+          setShowErrorBox(err.response.data);
+        }, 2000);
+      }
+    }
 
-    const coords = response.data.coord;
+    const coords = response?.data?.coord;
 
-    const { lon, lat } = coords;
+    const { lon, lat } = coords || {};
 
     const futureDaysData = await axios(
       `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=${API_KEY}&units=metric&lang=en`
@@ -54,6 +72,8 @@ function App() {
     fetchData,
     showLoader,
     setShowLoader,
+    showErrorBox,
+    setShowErrorBox,
   };
 
   return (
@@ -61,6 +81,10 @@ function App() {
       <BackgroundBox />
       <div className="App">
         {showLoader ? <LoadingBox showLoader={showLoader} /> : null}
+        <ErrorBox
+          showErrorBox={showErrorBox}
+          setShowErrorBox={setShowErrorBox}
+        />
         <GlobalStyle />
         <AppContext.Provider value={contextValue}>
           <Switch>
